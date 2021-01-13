6322b09a9e0ad851f9d26ae3eefded25a7ca9db6
github username <github email address>
2021-01-12 21:41:20 -0500

    changed methods

--- lib/drink_recipes/api.rb
+++ lib/drink_recipes/api.rb
@@ -1,6 +1,6 @@
 class DrinkRecipes::API
-    def self.get_categories
-        categories = RestClient.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    def self.get_drinks
+        drinks = RestClient.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         @drinks = JSON.parse(categories)
         # binding.pry
         @drinks["drinks"].each do |drink|
--- lib/drink_recipes/cli.rb
+++ lib/drink_recipes/cli.rb
@@ -1,14 +1,13 @@
 class DrinkRecipes::CLI
     def start
-        puts "Welcome to the drink recipe gem!"
-        DrinkRecipes::API.get_categories
+        puts "Welcome to the drink recipe gem! Please choose a drink"
+        #list choices from API
+        DrinkRecipes::API.new_drink_from_api
     end
 
-    def menu
-        puts "Please select a category."
-        list_categories
-    end
+   
 
-    def list_categories
+    def exit
+        puts "Thank you for using this app!"
     end
 end
