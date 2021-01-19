2f42f7fed25fc5b9a7424a57184782e183159859
github username <github email address>
2021-01-19 11:23:34 -0500

    fixed loop

--- lib/drink_recipes/cli.rb
+++ lib/drink_recipes/cli.rb
@@ -1,21 +1,19 @@
 class DrinkRecipes::CLI
 
-    def start
+    def self.start
         puts "Welcome! Please type in a drink."
         input = gets.strip
-        # # while input != "exit"
-        #     if input == "exit"
-        #         puts "Thank you for using this app!"
-        #  # binding.pry
-        #     else input = gets.strip
+        while input != "exit"
         DrinkRecipes::API.new_drink_from_api(input)
+        puts "Please input another drink or type exit to end."
+        input = gets.strip
         
+        end
+        self.goodbye
     end
 
    
-    # def ending
-    #     puts "Are you done? Type 'exit' to exit."
-    #     input = gets.strip
-    # end 
-
+   def self.goodbye
+    puts "Thank you and have a great day!"
+   end
 end
